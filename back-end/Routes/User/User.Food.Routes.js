a68781710b61eef5cea16574f41af71161088dad
Mohammed Eid Saad <ziadeid212@gmail.com>
2024-06-17 23:51:56 +0300

    Update User.Food.Routes.js
    user update

--- back-end/Routes/User/User.Food.Routes.js
+++ back-end/Routes/User/User.Food.Routes.js
@@ -2,9 +2,11 @@ const app = require("express").Router();
 const {
   GetAllFood,
   GetFood,
+  addMeal,
 } = require("../../controllers/User/Food.controller");
 
 app.get("/AllFood/:id", GetAllFood);
 app.post("/Food/:id", GetFood);
+app.post("/Food/AddMeal", addMeal);
 
 module.exports = app;
